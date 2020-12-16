c6bdc8ce2680873e78de9d1513fabbd1ec51d5ea
Kalib Hackin <naguihw@gmail.com>
2020-12-15 22:22:40 -0500

    styling car card

--- src/components/CarCard/index.jsx
+++ src/components/CarCard/index.jsx
@@ -9,12 +9,12 @@ const CarCard = () => (
       <img src="https://s3.caradvice.com.au/wp-content/uploads/2016/11/great-wall_wey_w01_06.jpg" alt="SUV" />
     </div>
     <div className="info">
-      <h3 className="price">$ 50.00</h3>
-      <h3 className="year">2018</h3>
+      <h3 className="item">$ 50.00</h3>
+      <h3 className="item">2018</h3>
     </div>
     <div className="options">
-      <Link to="/">More info</Link>
-      <Link to="/">Make a reservation</Link>
+      <Link to="/">More Info</Link>
+      <Link to="/">Make a Reservation</Link>
     </div>
   </div>
 );
