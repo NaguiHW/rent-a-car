a81bf2295e46780450a3397082e84b1e9a4a4e25
Kalib Hackin <naguihw@gmail.com>
2020-12-15 19:08:37 -0500

    creating car card

---
+++ src/components/CarCard/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.scss';
+
+const CarCard = () => (
+  <div className="car-card">
+    <h1 className="title">Car Name</h1>
+    <div className="image-container">
+      <img src="https://s3.caradvice.com.au/wp-content/uploads/2016/11/great-wall_wey_w01_06.jpg" alt="SUV" />
+    </div>
+    <div className="info">
+      <h3 className="price">$ 50.00</h3>
+      <h3 className="year">2018</h3>
+    </div>
+    <div className="options">
+      <Link to="/">More info</Link>
+      <Link to="/">Make a reservation</Link>
+    </div>
+  </div>
+);
+
+export default CarCard;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import CarCard from '../CarCard';
 import NavBar from '../NavBar';
+import './style.scss';
+
+const carTypes = ['All', 'SUV', 'Truck', 'Sedan', 'Van', 'Luxury Car', 'Sports Car'];
 
 const Home = () => (
   <div className="home">
     <NavBar />
+    <div className="content">
+      <div className="filters">
+        <select name="car-types" id="car-types">
+          {
+            carTypes.map(type => (
+              <option value={type} key={type}>{type}</option>
+            ))
+          }
+        </select>
+      </div>
+      <div className="cars">
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+      </div>
+    </div>
   </div>
 );
 
