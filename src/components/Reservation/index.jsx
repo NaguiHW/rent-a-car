62d48e73e09854e8dba4a9832d951045744f8d9c
Kalib Hackin <naguihw@gmail.com>
2020-12-19 06:18:01 -0500

    styling reservation form

--- src/components/Reservation/index.jsx
+++ src/components/Reservation/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import moment from 'moment';
+import './style.scss';
 
 const Reservation = () => {
   const { id } = useParams();
