e27d2815400206b4a8bd84e5c2407ebe868678e3
Kalib Hackin <naguihw@gmail.com>
2020-12-18 23:32:11 -0500

    styling form

--- src/components/ManageCars/index.jsx
+++ src/components/ManageCars/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 // import { storage } from '../../firebase';
-// import './style.scss';
+import './style.scss';
 
 const carTypes = ['SUV', 'Truck', 'Sedan', 'Van', 'Luxury Car', 'Sports Car'];
 const transmission = ['Auto', 'Manual'];
