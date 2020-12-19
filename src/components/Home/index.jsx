9c64af922260b9469b5f01f22e5b1ffdfd0f6463
Kalib Hackin <naguihw@gmail.com>
2020-12-19 04:34:02 -0500

    showing all cars in home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,27 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import CarCard from '../CarCard';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import './style.scss';
 
 const carTypes = ['All', 'SUV', 'Truck', 'Sedan', 'Van', 'Luxury Car', 'Sports Car'];
 
-const Home = ({ userStatus }) => (
-  <div className="home">
-    <div className="filters">
-      <select name="car-types" id="car-types">
+const Home = ({ userStatus }) => {
+  const [showCars, setShowCars] = useState([]);
+
+  const loadCars = () => {
+    axios.get('https://db-car.herokuapp.com/cars', { withCredentials: true })
+      .then(res => {
+        setShowCars(res.data.cars);
+      }).catch(err => {
+        console.error(err);
+      })
+  };
+
+  useEffect(() => {
+    loadCars();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="filters">
+        <select name="car-types" id="car-types">
+          {
+            carTypes.map(type => (
+              <option value={type} key={type}>{type}</option>
+            ))
+          }
+        </select>
+      </div>
+      <div className="cars">
         {
-          carTypes.map(type => (
-            <option value={type} key={type}>{type}</option>
+          showCars.map(car => (
+            <CarCard userStatus={userStatus} car={car} />
           ))
         }
-      </select>
+      </div>
     </div>
-    <div className="cars">
-      <CarCard userStatus={userStatus} />
-      <CarCard userStatus={userStatus} />
-    </div>
-  </div>
-);
+  );
+};
 
 Home.propTypes = {
   userStatus: PropTypes.bool.isRequired,
